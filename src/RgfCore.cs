292dc97a674925f995027677e98bec0b5c228e9c
Tamás Mikola <mikola.tamas@recrovit.hu>
2025-04-08 18:50:28 +0200

    Update version to 8.17.0 for project and RecroGrid

--- src/RgfCore.cs
+++ src/RgfCore.cs
@@ -1,5 +1,6 @@
-﻿
-namespace Recrovit.RecroGridFramework.Core;
+﻿namespace Recrovit.RecroGridFramework.Core;
+
+#pragma warning disable CS0618 // Type or member is obsolete
 
 public partial class RgfCore<TEntity> : RecroGridDBC<TEntity> where TEntity : class
 {
